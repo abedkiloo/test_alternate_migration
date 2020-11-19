d639aa9c81b3cad27a64bb4d15e37bfd3b411db6
Abednego Wambua <abedxh@gmail.com>
2020-11-19 15:23:01 +0300

    updated teting model

--- config/app.php
+++ config/app.php
@@ -165,6 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Gilbitron\Laravel\MigrateToBranchServiceProvider::class,
+
 
         /*
          * Application Service Providers...
